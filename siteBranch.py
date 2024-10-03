0c1e31262f817cd7da8c4670423e66cb56eba12e
Ruslan-Tagiev1005 <ruslantagiev21@gmail.com>
2024-10-03 11:48:40 +0300

    flaskapp

--- siteBranch.py
+++ siteBranch.py
@@ -1,2 +1,18 @@
-print('здеся будет сайт, дулиля')
-print('its over')
+from flask import Flask, render_template
+from pprint import pprint
+
+
+
+app = Flask(__name__)
+@app.route("/")
+def news_text():
+    with open("data.json") as f:
+        a = f.read()
+
+        text = a
+        template = '/news.html'
+    return render_template(template,text=text)
+
+if __name__ == "__main__":
+
+    app.run()
