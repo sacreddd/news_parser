f3fb54a3363f68d893e8d186922956be1a0e6761
sacreddd <savelev.vitaliy2003@mail.ru>
2024-10-03 12:15:07 +0300

    hourly parse

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import time
 import json
 from datetime import datetime
 
-from dateutil.utils import today
-
 url = "https://mtuci.ru/about_the_university/news/"
 
 PATTERN_OUT = "%d.%m.%y"
@@ -14,6 +12,7 @@ day = datetime.strftime(datetime.today().date(), PATTERN_OUT)
 
 
 def news_check():
+
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                              'Chrome/129.0.0.0 Safari/537.36'}
     startPagebs = BeautifulSoup(requests.get(url, headers=headers).text, "lxml")
@@ -23,17 +22,19 @@ def news_check():
         if item.find('p', class_="meta").text.strip() == day:
             # print(item.find('a').attrs['href'])
             currentNew_url = "https://mtuci.ru" + item.find('a').attrs['href']
-            # BeautifulSoup(requests.get(currentNew_url, headers=headers).text, "lxml").find('div', class_="news-single")
-            # print(BeautifulSoup(requests.get(currentNew_url, headers=headers).text, "lxml").find('div', class_="news-single").text)
             data_forj.append(BeautifulSoup(requests.get(currentNew_url, headers=headers).text, "lxml").find('div',
                                                                                                             class_="news-single").text)
-    try:
-        data = json.load(open('data.json'))
-    except:
-        data = []
-    data.append(data_forj)
+    # try:
+    #     data = json.load(open('data.json'))
+    # except:
+    #     data = []
+    # data.append(data_forj)
     with open('data.json', 'w', ) as file:
-        json.dump(data, file, indent=4, ensure_ascii=False)
+        json.dump(data_forj, file, indent=4, ensure_ascii=False)
 
 
-news_check()
+schedule.every().hour.do(news_check)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
