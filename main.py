3c42be815e0000ae7a675bc0f1fe0bcf6bce4993
sacreddd <savelev.vitaliy2003@mail.ru>
2024-10-04 13:09:43 +0300

    formatted json

--- main.py
+++ main.py
@@ -12,27 +12,27 @@ day = datetime.strftime(datetime.today().date(), PATTERN_OUT)
 
 
 def news_check():
-
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                              'Chrome/129.0.0.0 Safari/537.36'}
     startPagebs = BeautifulSoup(requests.get(url, headers=headers).text, "lxml")
     block = startPagebs.findAll('div', class_="news-list__item")
-    data_forj = []
+    data_forj = {}
     for item in block:
         if item.find('p', class_="meta").text.strip() == day:
-            # print(item.find('a').attrs['href'])
             currentNew_url = "https://mtuci.ru" + item.find('a').attrs['href']
-            data_forj.append(BeautifulSoup(requests.get(currentNew_url, headers=headers).text, "lxml").find('div',
-                                                                                                            class_="news-single").text)
-    # try:
-    #     data = json.load(open('data.json'))
-    # except:
-    #     data = []
-    # data.append(data_forj)
+            currentArticle = BeautifulSoup(requests.get(currentNew_url, headers=headers).text, "lxml").find('div',
+                                                                                                            class_="news-single")
+            p = []
+            for i in currentArticle.findAll('p'):
+                p.append(i.text.strip())
+            if p:
+                data_forj[str(currentArticle.find('h2', class_="text-center").text.strip())] = p
+
     with open('data.json', 'w', ) as file:
         json.dump(data_forj, file, indent=4, ensure_ascii=False)
 
 
+# news_check()
 schedule.every().hour.do(news_check)
 
 while True:
