33ba9509b067259abde311767ceef2bf6cc66b2f
sacreddd <savelev.vitaliy2003@mail.ru>
2024-10-06 16:09:27 +0300

    ff

---
+++ classN.py
@@ -0,0 +1,7 @@
+class NewsClass:
+    def __init__(self, university, date, header, image, text):
+        self.university = university
+        self. date = date
+        self.header = header
+        self.image = image
+        self.text = text
--- main.py
+++ main.py
@@ -4,8 +4,12 @@ import schedule
 import time
 import json
 from datetime import datetime
+from classN import NewsClass
+import  sqlite3
+
 
 url = "https://mtuci.ru/about_the_university/news/"
+
 
 PATTERN_OUT = "%d.%m.%y"
 day = datetime.strftime(datetime.today().date(), PATTERN_OUT)
@@ -16,7 +20,7 @@ def news_check():
                              'Chrome/129.0.0.0 Safari/537.36'}
     startPagebs = BeautifulSoup(requests.get(url, headers=headers).text, "lxml")
     block = startPagebs.findAll('div', class_="news-list__item")
-    data_forj = {}
+    data_forj = NewsClass(None, None, None, None, None)
     for item in block:
         if item.find('p', class_="meta").text.strip() == day:
             currentNew_url = "https://mtuci.ru" + item.find('a').attrs['href']
@@ -26,15 +30,20 @@ def news_check():
             for i in currentArticle.findAll('p'):
                 p.append(i.text.strip())
             if p:
-                data_forj[str(currentArticle.find('h2', class_="text-center").text.strip())] = p
+                data_forj.header = str(currentArticle.find('h2', class_="text-center").text.strip())
+                data_forj.text = p
+                data_forj.date = day
+                data_forj.university = 'МТУСИ'
+                data_forj.image = 'здесь будет картинка'
+
+                # data_forj[str(currentArticle.find('h2', class_="text-center").text.strip())] = p
 
     with open('data.json', 'w', ) as file:
         json.dump(data_forj, file, indent=4, ensure_ascii=False)
 
+news_check()
+# schedule.every().hour.do(news_check)
 
-# news_check()
-schedule.every().hour.do(news_check)
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
